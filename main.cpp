4e7d8f4e53b5e22e4173074b67f99cc61049d6fd
Andrew 'ImDoctor <andrew@imdoc.ru>
2025-09-04 18:55:13 +0300

    Make main and create Renderer class files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+// proj libs
+#include "Engine/Renderer.h"
+
+
+
+int main() {
+
+	setlocale(0, "rus");
+	std::cout << "Some text in the console" << std::endl;
+
+
+	system("pause>nul");
+	return 0;
+}
